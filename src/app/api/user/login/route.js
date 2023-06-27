a52ed41adb02fe489c0de03e11c6ba7510c2d928
Sharvil <sharvilm777@gmail.com>
2023-06-28 00:33:37 +0530

    Created .env file

---
+++ src/app/api/user/login/route.js
@@ -0,0 +1,9 @@
+const User = require("@/app/models/user");
+const connect_DB = require("@/app/utils/DBConnect");
+
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+export async function POST() {
+
+}
